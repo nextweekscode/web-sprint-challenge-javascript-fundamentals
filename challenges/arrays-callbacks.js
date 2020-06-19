a3645c81802bd69b28009a7d0c6eb6a16890073c
Ashton <ashtoturn@gmail.com>
2020-06-19 11:30:41 -0500

    finished

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -21,6 +21,13 @@ The zoos want to display both the scientific name and the animal name in front o
 
 */
 const displayNames = [];
+zooAnimals.forEach(function(item){
+ displayNames.push(item.scientific_name);
+ displayNames.push(item.animal_name);
+ 
+  
+  
+});
 console.log(displayNames);
 
 /* Request 2: .map()
@@ -30,6 +37,10 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 */
 
 const lowCaseAnimalNames
+zooAnimals.map(function(item){
+  return item.animal_name.toLowerCase();
+
+});
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
--- challenges/classes.js
+++ challenges/classes.js
@@ -1,7 +1,36 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
 
-// Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+class CubiodMaker{
+    constructor(attri){
+    this.length = attri.length;
+    this.width = attri.width;
+    this.height = attri.height;
+  }
+  volume = function(){
+    return this.length*this.width*this.height
+  }
+  surfaceArea = function(){
+    return ((this.length * this.width) + (this.length * this.height) + (this.width * this.height))
+  }
+}
 
+class CubeMaker extends CubiodMaker{
+    constructor(attri){
+        super(attri)
+    }
+    cubeVolume = function(){
+      return  this.length*this.width*this.height
+    }
+    cubeSurArea = function(){
+      return  (this.length*this.width*this.height)* 6
+    }
+}
+
+const cubeOne = new CubiodMaker(4, 5, 5);
+const cubeTwo = new CubeMaker(4, 4, 4);
+// Test your volume and surfaceArea methods by uncommenting the logs below:
+console.log(cubeOne.volume()); // 100
+console.log(cubeOne.surfaceArea()); // 130
+console.log(cubeTwo.cubeVolume());
+console.log(cubeTwo.cubeSurArea());
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
